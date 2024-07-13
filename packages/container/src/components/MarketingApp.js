e0a7f67593a296304ffd50e8b23b1bc6c1571784
Oleksandr Bezditnyi <oleksandrbezditny@gmail.com>
2024-07-13 09:59:49 +0200

    Navigation integration 2nd

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,18 +1,22 @@
-import { mount } from 'marketing/MarketingApp';
-import React, { useRef, useEffect } from 'react';
-import { useHistory } from "react-router-dom";
+import {mount} from 'marketing/MarketingApp';
+import React, {useRef, useEffect} from 'react';
+import {useHistory} from "react-router-dom";
 
 export default () => {
-  const ref = useRef(null);
-  const history = useHistory();
+    const ref = useRef(null);
+    const history = useHistory();
 
-  useEffect(() => {
-    mount(ref.current, {
-      onNavigate: ({ pathname: nextPathname }) => {
-        history.push(nextPathname);
-      }
+    useEffect(() => {
+        mount(ref.current, {
+            onNavigate: ({pathname: nextPathname}) => {
+                const {pathname} = history.pathname;
+
+                if (pathname !== nextPathname) {
+                    history.push(nextPathname);
+                }
+            }
+        });
     });
-  });
 
-  return <div ref={ref} />;
+    return <div ref={ref}/>;
 };
