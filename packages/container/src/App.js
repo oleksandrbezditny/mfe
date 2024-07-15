aa6810fa0af09be55bd8ff5cd9b4b221d49367d0
Oleksandr Bezditnyi <oleksandrbezditny@gmail.com>
2024-07-15 21:29:19 +0200

    fixed issue with routing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import MarketingApp from './components/MarketingApp';
 import Header from "./components/Header";
+import AuthApp from "./components/AuthApp";
 import {createGenerateClassName, StylesProvider} from "@material-ui/core";
 
 const generateClassName = createGenerateClassName({
@@ -14,7 +15,10 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header/>
-                    <MarketingApp/>
+                    <Switch>
+                        <Route path="/auth" component={AuthApp} />
+                        <Route path="/" component={MarketingApp} />
+                    </Switch>
                 </div>
             </StylesProvider>
         </BrowserRouter>
